51383519b200604a5086a5f827a2667ae8d9ca42
sudipeb <sudip.koirala@ebpearls.com>
2025-10-29 10:19:44 +0545

    getting the list of values on user action

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ void main() {
   runApp(const MyApp());
 }
 
+//initializing a list of names
 const name = [
   'Ram',
   'Shyam',
@@ -20,8 +21,39 @@ const name = [
   'Emily',
 ];
 
-class MyApp extends StatelessWidget {
+extension RandomElement<T> on Iterable<T> {
+  //getting random index number from the list
+  T getRandomElement() => elementAt(math.Random().nextInt(length));
+}
+
+//declaring the cubit class with the initial state as null and and state type as optional String
+class NamesCubit extends Cubit<String?> {
+  NamesCubit() : super(null);
+  //function to pick random name from the list and emit the new state
+  void pickRandomName() => emit(name.getRandomElement());
+}
+
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  late final NamesCubit nameCubit;
+  @override
+  void initState() {
+    super.initState();
+    nameCubit = NamesCubit(); //initializing the cubit for any statechanges
+  }
+
+  @override
+  void dispose() {
+    nameCubit
+        .close(); //closing the cubit to prevent memory leaks while state is not in use
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -29,7 +61,42 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData(primarySwatch: Colors.blue),
       title: 'cubit Demo',
-      home: Scaffold(appBar: AppBar(title: Text('Cubit Demo App'))),
+      home: Scaffold(
+        appBar: AppBar(title: Text('Cubit Demo App')),
+        body: Center(
+          //using StreamBuilder to listen to the stream of the cubit for any state changes
+          child: StreamBuilder<String?>(
+            //listening to the stream of the cubit we initialized
+            stream: nameCubit.stream,
+            //builder to build the UI based on the snapshot of the stream initially null
+            builder: (context, snapshot) {
+              final button = TextButton(
+                onPressed: () => nameCubit.pickRandomName(),
+                child: const Text("Pick random name"),
+              );
+              switch (snapshot.connectionState) {
+                case ConnectionState.none:
+                  return button;
+                case ConnectionState.waiting:
+                  return button;
+                case ConnectionState.active:
+                  return Center(
+                    child: Column(
+                      children: [
+                        Text(
+                          snapshot.data ?? 'No Names Picked',
+                        ), //displaying the picked random name from the snapshot data
+                        button,
+                      ],
+                    ),
+                  );
+                case ConnectionState.done:
+                  return const SizedBox();
+              }
+            },
+          ),
+        ),
+      ),
     );
   }
 }
